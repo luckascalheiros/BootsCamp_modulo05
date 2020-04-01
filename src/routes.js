1b2f537eb62fe3102f432958f031d69d7a1ecbdc
Lucas Calheiros <luccascalheiros@gmail.com>
2020-04-01 17:08:48 -0300

    add react-router-dom e roteamento

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Main from './pages/Main';
+import Repository from './pages/Repository';
+
+function Routes() {
+    return (
+        <div>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" exact component={Main} />
+                    <Route path="/repository" component={Repository} />
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default Routes;
